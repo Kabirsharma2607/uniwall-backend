8ffb2672dd0733b7fab1e6b6f2e3d2232f4a5aa8
Kabirsharma2607 <kabirsharma2607@gmail.com>
2025-03-29 22:42:40 +0530

    added signup route

--- backend/src/router/auth/route.ts
+++ backend/src/router/auth/route.ts
@@ -1,12 +1,16 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { PrismaClient } from "@prisma/client";
 
 const router = Router();
 
 const prisma = new PrismaClient();
 
-router.get("/login", (req, res) => {
+router.get("/login", async (req: Request, res: Response) => {
   res.json({ message: "Login successful!" });
+});
+
+router.post("/signup", (req: Request, res: Response) => {
+  const { body } = req;
 });
 
 export const authRouter = router;
