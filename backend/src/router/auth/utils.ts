47e01c7c5077740af92625de292792a9eb141c41
Kabirsharma2607 <kabirsharma2607@gmail.com>
2025-04-01 22:44:14 +0530

    added codeowners

---
+++ backend/src/router/auth/utils.ts
@@ -0,0 +1,58 @@
+import bcrypt from "bcrypt";
+import { nanoid } from "nanoid";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import dayjs from "dayjs";
+
+dotenv.config();
+
+const saltRounds = Number(process.env.SALT_ROUNDS);
+
+export const generateHash = async (data: string) => {
+  const hashed = await bcrypt.hash(data, saltRounds);
+  return hashed;
+};
+
+export const comparePasswords = async (
+  password: string,
+  hashedPassword: string
+) => {
+  return await bcrypt.compare(password, hashedPassword);
+};
+
+const JWT_SECRET = process.env.JWT_SECRET || "default-wont-work";
+const JWT_EXPIRY = 30 * 60;
+export const generateAuthToken = (userId: string, username: string) => {
+  return jwt.sign({ userId, username }, JWT_SECRET, { expiresIn: JWT_EXPIRY });
+};
+
+export const generate24WordHash = () => {
+  const wordLength = 4;
+  const totalWords = 24;
+
+  const generateWord = (): string => {
+    return nanoid(wordLength);
+  };
+
+  return Array.from({ length: totalWords }, generateWord);
+};
+
+const isValidSecret = (secret: string[]): boolean => {
+  return secret.every((word) => word.length === 4);
+};
+
+export const compareSecrets = (secretWord: string[], userOldSecret: string) => {
+  if (!isValidSecret(secretWord)) return false;
+
+  const userSecretArray = userOldSecret.split("-");
+  if (!isValidSecret(userSecretArray)) return false;
+
+  return JSON.stringify(secretWord) === JSON.stringify(userSecretArray);
+};
+
+export function canUpdatePassword(lastUpdate: Date): boolean {
+  if (!lastUpdate) return true;
+
+  const oneDayAgo = dayjs().subtract(1, "day");
+  return dayjs(lastUpdate).isBefore(oneDayAgo);
+}
---
+++ common/src/index.ts
@@ -0,0 +1,25 @@
+import * as z from "zod";
+
+export const authSchema = z.object({
+  username: z.string().min(6).max(15),
+  password: z.string().min(8).max(20),
+});
+
+export type AuthSchemaType = z.infer<typeof authSchema>;
+
+export const forgotPasswordSchema = z.object({
+  username: z.string().min(6).max(15),
+  secretWord: z.string().array().length(24),
+  newPassword: z.string().min(8).max(20),
+});
+
+export type ForgotPasswordSchema = z.infer<typeof forgotPasswordSchema>;
+
+export const resetPasswordSchema = z.object({
+  username: z.string().min(6).max(15),
+  oldPassword: z.string().min(8).max(20),
+  newPassword: z.string().min(8).max(20),
+  secretWord: z.string().array().length(24),
+});
+
+export type ResetPasswordSchema = z.infer<typeof resetPasswordSchema>;
