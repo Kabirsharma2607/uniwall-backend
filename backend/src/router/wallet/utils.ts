e954f5fdc04641638367220f612752932715a036
Kabirsharma2607 <kabirsharma2607@gmail.com>
2025-04-24 00:42:07 +0530

    removed logs

--- backend/src/router/wallet/utils.ts
+++ backend/src/router/wallet/utils.ts
@@ -6,29 +6,31 @@ import { createBitcoinWallet } from "../../wallet-functions/bitcoin";
 import { WalletType } from "@kabir.26/uniwall-commons";
 
 type GeneratedWalletKeyPairsType = {
-    walletType: WalletType;
-    keyPair: GeneratedWalletType;
+  walletType: WalletType;
+  keyPair: GeneratedWalletType;
 };
 
 export const createWallets = async (requestedWallets: WalletType[]) => {
-    const response: GeneratedWalletKeyPairsType[] = [];
+  const response: GeneratedWalletKeyPairsType[] = [];
 
-    for (const wallet of requestedWallets) {
-        switch (wallet) {
-            case "SOL":
-                response.push({ walletType: wallet, keyPair: createSolanaWallet() });
-                break;
-            case "ETH":
-                response.push({ walletType: wallet, keyPair: createEthereumWallet() });
-                break;
-            case "PALO":
-                response.push({ walletType: wallet, keyPair: await createPolkadotWallet() });
-                break;
-            case "BTC":
-                response.push({ walletType: wallet, keyPair: createBitcoinWallet() });
-                break;
-        }
+  for (const wallet of requestedWallets) {
+    switch (wallet) {
+      case "SOL":
+        response.push({ walletType: wallet, keyPair: createSolanaWallet() });
+        break;
+      case "ETH":
+        response.push({ walletType: wallet, keyPair: createEthereumWallet() });
+        break;
+      case "PALO":
+        response.push({
+          walletType: wallet,
+          keyPair: await createPolkadotWallet(),
+        });
+        break;
+      case "BTC":
+        response.push({ walletType: wallet, keyPair: createBitcoinWallet() });
+        break;
     }
-    console.log(response);
-    return response;
+  }
+  return response;
 };
